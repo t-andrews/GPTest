21d0f68e1b91fc331aaa57fd067a2ca73cdfcd8e
GitHub Action <action@github.com>
2023-01-09 05:06:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/tandrews/gptest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/tandrews/gptest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.tandrews.gptest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tandrews.gptest.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/tandrews/gptest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tandrews/gptest/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.tandrews.gptest.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tandrews.gptest.MyBundle
 
 class MyProjectService(project: Project) {
 
